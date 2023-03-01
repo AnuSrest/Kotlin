a80186eab4508ae7459784ad2a10c3c996b4530d
Anu Shrestha <psrest789@gmail.com>
2023-03-01 08:07:23 +0545

    Initial  Commit

---
+++ app/src/main/java/com/example/assignment/ItemsViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.assignment
+
+data class ItemsViewModel(val image: Int, val text: String){
+
+}
+
---
+++ app/src/main/java/com/example/assignment/Retrofit.kt
@@ -0,0 +1,21 @@
+package com.example.assignment
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Retrofit {
+
+
+    object RetrofitHelper {
+
+        val baseUrl = "https://quotable.io/"
+
+
+        fun getInstance(): Retrofit {
+            return Retrofit.Builder().baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                // we need to add converter factory to
+                // convert JSON object to Java object
+                .build()
+        }
+    }
+}
